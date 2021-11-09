4d07f4f513210a3dde047a53d034ca80e0abb536
Adam Field <adamjfield@gmail.com>
2021-11-08 22:52:52 -0600

    generate HTML file

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log("================");
+const pageHTML = generatePage(name, github);
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
 
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
